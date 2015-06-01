7c6a47fe838f68ad28d65ac8657f3c55ad9e4ce1
Robbin van den berg <robbinvandenberg@hotmail.com>
2015-06-01 22:49:05 +0200

    meetrapporten

--- source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
+++ source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
@@ -5,7 +5,139 @@ bool StudentLocalization::stepFindHead(const IntensityImage &image, FeatureMap &
 }
 
 bool StudentLocalization::stepFindNoseMouthAndChin(const IntensityImage &image, FeatureMap &features) const {
-	return false;
+	// magic
+
+	int topOfHead = (int)features.getFeature(Feature::FEATURE_HEAD_TOP).getY();
+	int rightOfHead = (int)features.getFeature(Feature::FEATURE_HEAD_RIGHT_SIDE).getX();
+	int leftOfHead = (int)features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getX();
+	int widthOfHead = std::abs(leftOfHead - rightOfHead);
+
+	int leftOfHeadY = (int)features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getY();
+	int headMidle = (widthOfHead / 2) + leftOfHead;
+	int partOfhead = (widthOfHead / 3);
+
+
+
+
+
+	//int headTop = (int)features.getFeature(Feature::FEATURE_HEAD_TOP).getY();
+	//int headLeft = (int)features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getX();
+	//int headRight = (int)features.getFeature(Feature::FEATURE_HEAD_RIGHT_SIDE).getX();
+
+	//int headWidth = std::abs(headLeft - headRight);
+	//int OverHillOverDale = (int)features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getY();
+
+
+	//int headMidle = headLeft + (headWidth / 2);
+	//int ThoroughBushThoroughBrier = (headWidth / 3);
+
+
+	/*SonnetXVIII OverParkOverPale = HereBeDragons::YetWhoKnowsNotConscienceIsBornOfLove(image, headMidle - (ThoroughBushThoroughBrier / 2), 0, ThoroughBushThoroughBrier, image.getHeight());
+
+	OverParkOverPale.SoLongAsMenCanBreathOrEyesCanSee((int)(OverParkOverPale.SometimeTooHotTheEyeOfHeavenShines() * 0.6), 0, -1);
+
+	if (OverParkOverPale[OverHillOverDale] == 0){
+		for (int i = OverHillOverDale + 1; i < OverParkOverPale.ThouArtMoreLovelyAndMoreTemperate(); i++){
+			if (OverParkOverPale[i] != 0){
+				OverHillOverDale = i;
+				break;
+			}
+		}
+	}
+
+
+	int ThoroughFloodThoroughFire = -1, IDoWanderEverywhere = -1, SwifterThanTheMoonsSphere = -1, AndIServeTheFairyQueen = -1;
+
+	int ToDewHerOrbsUponTheGreen = -1;
+
+	int TheCowslipsTallHerPensionersBe = 0;
+	int InTheirGoldCoatsSpotsYouSee = 1;
+	for (int i = OverHillOverDale; i < OverParkOverPale.ThouArtMoreLovelyAndMoreTemperate(); i++){
+
+		if (OverParkOverPale[i] == 0 && InTheirGoldCoatsSpotsYouSee == 1){
+			if (TheCowslipsTallHerPensionersBe == 0){
+				ThoroughFloodThoroughFire = i - 1;
+			}
+			else if (TheCowslipsTallHerPensionersBe == 1){
+				SwifterThanTheMoonsSphere = i - 1;
+			}
+			TheCowslipsTallHerPensionersBe++;
+			InTheirGoldCoatsSpotsYouSee = 0;
+
+		}
+		else if (OverParkOverPale[i] != 0 && InTheirGoldCoatsSpotsYouSee == 0){
+			if (TheCowslipsTallHerPensionersBe == 1){
+				IDoWanderEverywhere = i;
+				ToDewHerOrbsUponTheGreen = IDoWanderEverywhere - ThoroughFloodThoroughFire;
+			}
+			else if (TheCowslipsTallHerPensionersBe == 2){
+
+				int ThoseBeRubiesFairyFavours = i - SwifterThanTheMoonsSphere;
+				if (ThoseBeRubiesFairyFavours < ToDewHerOrbsUponTheGreen){
+					TheCowslipsTallHerPensionersBe = 1;
+				}
+				else {
+					AndIServeTheFairyQueen = i;
+				}
+			}
+			InTheirGoldCoatsSpotsYouSee = 1;
+		}
+	}
+
+
+	int InThoseFrecklesLiveTheirSavours = AndIServeTheFairyQueen;
+	int IMustGoSeekSomeDewdropsHere = OverParkOverPale[AndIServeTheFairyQueen];
+	if (AndIServeTheFairyQueen != -1){
+		for (int i = AndIServeTheFairyQueen; i < OverParkOverPale.ThouArtMoreLovelyAndMoreTemperate(); i++){
+			if (OverParkOverPale[i] == 0){
+				break;
+			}
+			else if (OverParkOverPale[i] > IMustGoSeekSomeDewdropsHere) {
+				InThoseFrecklesLiveTheirSavours = i;
+				IMustGoSeekSomeDewdropsHere = OverParkOverPale[i];
+			}
+		}
+	}
+
+	AndIServeTheFairyQueen = InThoseFrecklesLiveTheirSavours;
+
+
+
+	if (ThoroughFloodThoroughFire != -1){
+		features.putFeature(Feature(Feature::FEATURE_NOSE_BOTTOM, Point2D<double>(headMidle, ThoroughFloodThoroughFire)));
+	}
+
+	if (IDoWanderEverywhere != -1) {
+		features.putFeature(Feature(Feature::FEATURE_MOUTH_TOP, Point2D<double>(headMidle, IDoWanderEverywhere)));
+	}
+
+	int AndHangAPearlInEveryCowslipsEar;
+	if (SwifterThanTheMoonsSphere == -1 || IDoWanderEverywhere == -1) {
+		AndHangAPearlInEveryCowslipsEar = -1;
+	}
+	else {
+		AndHangAPearlInEveryCowslipsEar = (IDoWanderEverywhere + SwifterThanTheMoonsSphere) / 2;
+	}
+	if (AndHangAPearlInEveryCowslipsEar != -1) {
+		features.putFeature(Feature(Feature::FEATURE_MOUTH_CENTER, Point2D<double>(headMidle, AndHangAPearlInEveryCowslipsEar)));
+	}
+
+	if (SwifterThanTheMoonsSphere != -1) {
+		features.putFeature(Feature(Feature::FEATURE_MOUTH_BOTTOM, Point2D<double>(headMidle, SwifterThanTheMoonsSphere)));
+	}
+
+	if (AndIServeTheFairyQueen != -1){
+		features.putFeature(Feature(Feature::FEATURE_CHIN, Point2D<double>(headMidle, AndIServeTheFairyQueen)));
+	}
+
+	if (AndIServeTheFairyQueen == -1 || AndHangAPearlInEveryCowslipsEar == -1 || ThoroughFloodThoroughFire == -1){
+		return false;
+	}
+	else {
+		return true;
+	}*/
+
+	return true;
 }
 
 bool StudentLocalization::stepFindChinContours(const IntensityImage &image, FeatureMap &features) const {
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -33,6 +33,11 @@ IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &imag
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
+
+	BaseTimer bt;
+	// start de timer
+	bt.start();
+
 	double imageWidth = image.getWidth();
 	double imageHeight = image.getHeight();
 	double oldDimensions = imageWidth * imageHeight;
@@ -60,9 +65,6 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 		}
 
 		newDimensions = newWidth * newHeight;
-
-		std::cout << newWidth << " x " << newHeight;
-
 
 		cop = ImageFactory::newIntensityImage();
 		cop->set(newWidth, newHeight);
@@ -98,7 +100,6 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 			}
 		}
 		
-
 	}
 	else {
 		// return original
@@ -106,18 +107,22 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 
 	}
 
+	// stop de timer
+	bt.stop();
+
+	// rapporteer door de elapsed...() functies aan te roepen
+	std::cout << "Scale Time for the operation was: " << bt.elapsedMicroSeconds() << " microseconds" << std::endl;
+
 	return cop;
 
 }
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
-	//double gaussianMask[5][5];		// Gaussian mask
-	double gaussianMask[9][9];
-	//double sum = 159.0;
-	double sum = 4.0;
 
-	int iOffset;
-	int newPixel;			// Sum pixel values for gaussian
+	BaseTimer bt;
+	// start de timer
+	bt.start();
+
 	int imageWidth = image.getWidth();
 	int imageHeight = image.getHeight();
 	IntensityImage * cop = ImageFactory::newIntensityImage();
@@ -139,7 +144,6 @@ IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &i
 	GY[2][0] = -1; GY[2][1] = -2; GY[2][2] = -1;
 
 	int SUM;
-	unsigned int val1, val2, val3;
 
 	//SOBEL edge detector implementation. 
 	for (int i = 0; i < imageHeight; i++)
@@ -159,6 +163,7 @@ IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &i
 			{
 				valX = 0;
 				valY = 0;
+
 				//calculating the X and Y convolutions
 				for (int x = -1; x <= 1; x++)
 				{
@@ -170,27 +175,22 @@ IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &i
 					}
 				}
 
-				//SUM = abs(valX) + abs(valY);
-
 			}
 
 			SUM = abs(valX) + abs(valY);
 			SUM = SUM > 255 ? 255 : SUM;
 			SUM = SUM < 0 ? 0 : SUM;
 
-			//Gradient magnitude
-			//val1 = sqrt(valX*valX + valY*valY);
-
-			//if (SUM>255) SUM = 255;
-			//if (SUM<0) SUM = 0;
-			//unsigned char tst = 255 - (unsigned char)(SUM);
-
 			cop->setPixel(j, i, SUM);
 
 		}
 	}
 
-	std::cout << "klaar";
+	// stop de timer
+	bt.stop();
+
+	// rapporteer door de elapsed...() functies aan te roepen
+	std::cout << "Edge detection Time for the operation was: " << bt.elapsedMicroSeconds() << " microseconds" << std::endl;
 
 	return cop;
 
@@ -218,153 +218,3 @@ IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &im
 
 	return cop;
 }
-
-///// end -> BACKUP BELOW
-//
-//#include "StudentPreProcessing.h"
-//#include "IntensityImageStudent.h"
-//#include "basetimer.h"
-//#include <iostream>
-//#include "ImageFactory.h"
-//#include "ImageIO.h"
-//
-//
-//IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-//	// maak een timer object aan
-//	BaseTimer bt;
-//
-//	IntensityImageStudent *intensity_image = new IntensityImageStudent(image.getWidth(), image.getHeight());
-//
-//	// start de timer
-//	bt.start();
-//
-//	for (int i = 0; i < image.getWidth()*image.getHeight(); i++){
-//		//Luminosity formula 0.21 R + 0.72 G + 0.07 B
-//		unsigned char sum = (image.getPixel(i).r * 0.21) + (image.getPixel(i).g * 0.72) + (image.getPixel(i).b * 0.07);
-//		//unsigned char sum = (image.getPixel(i).r  + image.getPixel(i).g  + image.getPixel(i).b) / 3;
-//
-//		Intensity it = sum;
-//		intensity_image->setPixel(i, it);
-//	}
-//
-//	// stop de timer
-//	bt.stop();
-//
-//	// rapporteer door de elapsed...() functies aan te roepen
-//	std::cout << "Time for the operation was: " << bt.elapsedMicroSeconds() << " microseconds" << std::endl;
-//
-//	return intensity_image;
-//}
-//
-//IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
-//	return nullptr;
-//}
-//
-//IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
-//	//double gaussianMask[5][5];		// Gaussian mask
-//	double gaussianMask[9][9];
-//	//double sum = 159.0;
-//	double sum = 4.0;
-//
-//	int iOffset;
-//	int newPixel;			// Sum pixel values for gaussian
-//	int imageWidth = image.getWidth();
-//	int imageHeight = image.getHeight();
-//	IntensityImage * cop = ImageFactory::newIntensityImage();
-//	cop->set(imageWidth, imageHeight);
-//
-//
-//	/* Declare Gaussian mask */
-//	/*gaussianMask[0][0] = 2;	 gaussianMask[0][1] = 4;  gaussianMask[0][2] = 5;  gaussianMask[0][3] = 4;  gaussianMask[0][4] = 2;
-//	gaussianMask[1][0] = 4;	 gaussianMask[1][1] = 9;  gaussianMask[1][2] = 12; gaussianMask[1][3] = 9;  gaussianMask[1][4] = 4;
-//	gaussianMask[2][0] = 5;	 gaussianMask[2][1] = 12; gaussianMask[2][2] = 15; gaussianMask[2][3] = 12; gaussianMask[2][4] = 2;
-//	gaussianMask[3][0] = 4;	 gaussianMask[3][1] = 9;  gaussianMask[3][2] = 12; gaussianMask[3][3] = 9;  gaussianMask[3][4] = 4;
-//	gaussianMask[4][0] = 2;	 gaussianMask[4][1] = 4;  gaussianMask[4][2] = 5;  gaussianMask[4][3] = 4;  gaussianMask[4][4] = 2;*/
-//
-//	gaussianMask[0][0] = 0;	 gaussianMask[0][1] = 0;  gaussianMask[0][2] = 3;  gaussianMask[0][3] = 2;		gaussianMask[0][4] = 2;		gaussianMask[0][5] = 2;			gaussianMask[0][6] = 3;  gaussianMask[0][7] = 0;  gaussianMask[0][8] = 0;
-//	gaussianMask[1][0] = 0;	 gaussianMask[1][1] = 2;  gaussianMask[1][2] = 3;  gaussianMask[1][3] = 5;		gaussianMask[1][4] = 5;		gaussianMask[1][5] = 5;			gaussianMask[1][6] = 3;  gaussianMask[1][7] = 2;  gaussianMask[1][8] = 0;
-//	gaussianMask[2][0] = 3;	 gaussianMask[2][1] = 3;  gaussianMask[2][2] = 5;  gaussianMask[2][3] = 3;		gaussianMask[2][4] = 0;		gaussianMask[2][5] = 3;			gaussianMask[2][6] = 5;  gaussianMask[2][7] = 3;  gaussianMask[2][8] = 3;
-//	gaussianMask[3][0] = 2;	 gaussianMask[3][1] = 5;  gaussianMask[3][2] = 3;  gaussianMask[3][3] = -12;	gaussianMask[3][4] = -23;	gaussianMask[3][5] = -12;		gaussianMask[3][6] = 3;  gaussianMask[3][7] = 5;  gaussianMask[3][8] = 2;
-//	gaussianMask[4][0] = 2;	 gaussianMask[4][1] = 5;  gaussianMask[4][2] = 0;  gaussianMask[4][3] = -23;	gaussianMask[4][4] = -40;	gaussianMask[4][5] = -23;		gaussianMask[4][6] = 0;  gaussianMask[4][7] = 5;  gaussianMask[4][8] = 2;
-//	gaussianMask[5][0] = 2;	 gaussianMask[5][1] = 5;  gaussianMask[5][2] = 3;  gaussianMask[5][3] = -12;	gaussianMask[5][4] = -23;	gaussianMask[5][5] = 12;		gaussianMask[5][6] = 3;  gaussianMask[5][7] = 5;  gaussianMask[5][8] = 2;
-//	gaussianMask[6][0] = 3;	 gaussianMask[6][1] = 3;  gaussianMask[6][2] = 5;  gaussianMask[6][3] = 3;		gaussianMask[6][4] = 0;		gaussianMask[6][5] = 3;			gaussianMask[6][6] = 5;  gaussianMask[6][7] = 3;  gaussianMask[6][8] = 3;
-//	gaussianMask[7][0] = 0;	 gaussianMask[7][1] = 2;  gaussianMask[7][2] = 3;  gaussianMask[7][3] = 5;		gaussianMask[7][4] = 5;		gaussianMask[7][5] = 5;			gaussianMask[7][6] = 3;  gaussianMask[7][7] = 2;  gaussianMask[7][8] = 0;
-//	gaussianMask[8][0] = 0;	 gaussianMask[8][1] = 0;  gaussianMask[8][2] = 3;  gaussianMask[8][3] = 2;		gaussianMask[8][4] = 2;		gaussianMask[8][5] = 2;			gaussianMask[8][6] = 3;  gaussianMask[8][7] = 0;  gaussianMask[8][8] = 0;
-//
-//
-//	/*for(int i = 0; i < 5; ++i) {
-//	for (int j = 0; j < 5; ++j) {
-//	gaussianMask[i][j] /= sum;
-//	std::cout << gaussianMask[i][j] << '\t';
-//	}
-//	std::cout << std::endl;
-//	}*/
-//
-//	for (int i = 0; i < 9; ++i) {
-//		for (int j = 0; j < 9; ++j) {
-//			gaussianMask[i][j] /= sum;
-//			std::cout << gaussianMask[i][j] << '\t';
-//		}
-//		std::cout << std::endl;
-//	}
-//
-//	// Gaussian blur
-//	// loop image pixels
-//	//for (int row = 2; row < imageHeight - 2; row++) {
-//	//	for (int col = 2; col < imageWidth - 2; col++) {
-//	//		newPixel = 0;
-//	//		// loop kernel
-//	//		for (int rowOffset = -2; rowOffset <= 2; rowOffset++) {
-//	//			for (int colOffset = -2; colOffset <= 2; colOffset++) {
-//	//				//int rowTotal = row + rowOffset;
-//	//				//int colTotal = col + colOffset;
-//	//				//iOffset = (rowTotal * 3 * imageWidth + colTotal * 3);
-//	//				newPixel += image.getPixel(row - colOffset, col - rowOffset) * gaussianMask[2 + rowOffset][2 + colOffset];
-//
-//	//			}
-//	//		}
-//
-//	//		//int z = (row * 3 * imageWidth + col * 3);
-//	//		//Intensity inten = newPixel / 159;
-//	//		Intensity inten = newPixel;
-//	//		//int posx = ((row-2)*imageWidth) + (col - 2);
-//	//		//int posy = row;
-//	//		cop->setPixel(row, col, inten);
-//
-//	//	}
-//	//}
-//
-//	for (int row = 4; row < imageHeight - 4; row++) {
-//		for (int col = 4; col < imageWidth - 4; col++) {
-//			newPixel = 0;
-//			// loop kernel
-//			for (int rowOffset = -4; rowOffset <= 4; rowOffset++) {
-//				for (int colOffset = -4; colOffset <= 4; colOffset++) {
-//					//int rowTotal = row + rowOffset;
-//					//int colTotal = col + colOffset;
-//					//iOffset = (rowTotal * 3 * imageWidth + colTotal * 3);
-//					newPixel += image.getPixel(row - colOffset, col - rowOffset) * gaussianMask[4 + rowOffset][4 + colOffset];
-//
-//				}
-//			}
-//
-//			//int z = (row * 3 * imageWidth + col * 3);
-//			//Intensity inten = newPixel / 159;
-//			Intensity inten = newPixel;
-//			//int posx = ((row-2)*imageWidth) + (col - 2);
-//			//int posy = row;
-//			cop->setPixel(row, col, inten);
-//
-//		}
-//	}
-//
-//	std::cout << "klaar";
-//
-//	return cop;
-//
-//	//return nullptr;
-//}
-//
-//IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
-//	return nullptr;
-//}
